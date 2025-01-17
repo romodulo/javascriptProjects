be74b2c0e3089009c90c73bd8e5c0cbc875ae2a5
romel-romodulo <57033860+romodulo@users.noreply.github.com>
2025-01-18 03:48:07 +1000

    first commit of <1-18>: checkpoint: <03:47>

--- testPerlinPython.py
+++ testPerlinPython.py
@@ -1,2 +1,7 @@
 from perlin_noise import PerlinNoise
 
+noise = PerlinNoise()
+
+objectA = noise([0.5, 0.5])
+objectB = noise([2.5, 5.5])
+print(objectA, objectB, objectA==objectB)
---
+++ testPerlinPythonWithExample.py
@@ -0,0 +1,66 @@
+import svg
+from perlin_noise import PerlinNoise
+
+sw = 500
+# nc = None
+cw = int(500/20)
+# print(int(cw))
+
+
+
+#Circle has a radius of 10
+
+
+
+elems = [] #test-list
+elements = [] 
+for x in range(0,21):
+    for y in range(0,21):
+        elems.append((x,y)) 
+        circlePX=x*cw
+        circlePY=y*cw
+        elements.append(svg.Circle(
+            cx=circlePX, cy=circlePY, r=cw,
+            # stroke="white", # creates a stroke (a.k.a a border)
+            fill="black"
+            # stroke_width=5 # stroke-width: self-explanatory
+            ))
+        # print(x,y)
+
+noise = PerlinNoise()
+cw = sw/21
+for y in range(0,20):
+    for x in range(0,20):
+        # elements.append(svg.Circle(
+        #     cx=cw*x, cy=cw*y, r=cw,
+        #     stroke="red",
+        #     stroke_width=1
+        # ))
+        print(str(cw*x+cw/2) + ", " + str(cw*y+cw/2))
+        elements.append(svg.Circle(
+            cx=cw*x+cw/2, cy=cw*y+cw/2, r=cw/2*noise([cw*x+cw/2, cw*y+cw/2]),
+            stroke="white",
+            stroke_width=1
+        ))
+
+        continue
+# test loop
+# for i in elems:
+#     print(i)
+# test loop
+# for i in elements:
+#     print(i)
+
+canvas = svg.SVG(
+    width = sw,
+    height = sw,
+    elements= elements
+)
+
+filename = "6th"
+
+fileOpen = open(f"./svgsFolder/{filename}.svg", "w")
+fileOpen.write(str(canvas))
+fileOpen.close()
+
+# print(noise([41.66, 41.66]))
