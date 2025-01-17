130652133ccdcd34a9a1a0437d7e697572baa61f
romel-romodulo <57033860+romodulo@users.noreply.github.com>
2025-01-18 01:57:30 +1000

    update: <1:57>

--- mainPython.py
+++ mainPython.py
@@ -3,7 +3,7 @@ import svg
 sw = 500
 # nc = None
 cw = int(500/20)
-print(int(cw))
+# print(int(cw))
 
 
 
@@ -13,8 +13,8 @@ print(int(cw))
 
 elems = [] #test-list
 elements = [] 
-for x in range(0,100):
-    for y in range(0,100):
+for x in range(0,21):
+    for y in range(0,21):
         elems.append((x,y)) 
         circlePX=x*cw
         circlePY=y*cw
@@ -25,6 +25,22 @@ for x in range(0,100):
             # stroke_width=5 # stroke-width: self-explanatory
             ))
         # print(x,y)
+# preliminary-data:
+cw = sw/5
+for y in range(0,5):
+    for x in range(0,5):
+        # elements.append(svg.Circle(
+        #     cx=cw*x, cy=cw*y, r=cw,
+        #     stroke="red",
+        #     stroke_width=1
+        # ))
+        elements.append(svg.Circle(
+            cx=cw*x+cw/2, cy=cw*y+cw/2, r=cw/2,
+            stroke="white",
+            stroke_width=1
+        ))
+
+        continue
 # test loop
 # for i in elems:
 #     print(i)
@@ -38,6 +54,8 @@ canvas = svg.SVG(
     elements= elements
 )
 
-fileOpen = open("test.svg", "w")
+filename = "5th"
+
+fileOpen = open(f"./svgsFolder/{filename}.svg", "w")
 fileOpen.write(str(canvas))
 fileOpen.close()
---
+++ testPerlinPython.py
@@ -0,0 +1,2 @@
+from perlin_noise import PerlinNoise
+
